4c4f072f8e942e2fed504e317d17737bbbb7152e
Nurfitra Pujo Santiko <nurfitra.pujo@mekari.com>
2022-07-05 23:54:51 +0700

    feat: implemented active nav

--- src/_components/shared/nav_link_component.rb
+++ src/_components/shared/nav_link_component.rb
@@ -1,12 +1,13 @@
-class Shared::NavLinkComponent
-  def initialize(href:, text:)
+class Shared::NavLinkComponent < Bridgetown::Component
+  def initialize(href:, text:, resource:)
     @href = href
     @text = text
+    @resource = resource
   end
 
-  def render_in(_view_context)
-    <<~HTML
-      <li><a href="#{@href}">#{@text}</a></li>
-    HTML
+  def is_active?
+    return @href == @resource.data.link unless @resource.data.link.nil?
+
+    @href[1..-1] == @resource.data.slug.downcase
   end
 end
